00c32ec5176b2e58fe7c930efa978060a52cdd59
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-05-01 06:36:35 +0530

    updated again

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2020/05/09',function(event){
+     jQuery('#clock').countdown('2020/05/09 12:00:00',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> <b>:</b> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> <b>:</b> Hours</div> '
